8c62bfca286382f7eb0a84aef5fca42e180d367f
hamidsalemi77 <48052326+hamidsalemi77@users.noreply.github.com>
2020-12-10 20:14:29 +0330

    Update Hazard_Detection_Unit.sv

--- Hazard_Detection_Unit.sv
+++ Hazard_Detection_Unit.sv
@@ -1,1 +1,46 @@
+module Hazard_Unit(
+    input rst,
+    input [3:0] src1, src2, EXE_Dest,
+    input EXE_WB_EN,
+    input [3:0] Mem_Dest,
+    input MEM_WB_EN,
+    input two_src,
+    
+    
+    
+    output hazard_detection
+);
+
+  reg out;
+  
+  assign hazard_detection = out;
+  
+  always@(*)begin
+    if(rst)
+      out = 0;
+    else begin
+      
+			if(two_src)
+				if(((EXE_WB_EN & (src2 == EXE_Dest)) | (MEM_WB_EN & (src2 == Mem_Dest)) | (MEM_WB_EN & (src1 == Mem_Dest)) | (EXE_WB_EN & (src1 == EXE_Dest))))begin
+					out = 1'b1;
+				end
+				else  	
+					out = 0;
+			else
+				if(((MEM_WB_EN & (src1 == Mem_Dest)) | (EXE_WB_EN & (src1 == EXE_Dest))))begin
+					out = 1'b1;
+				end
+				else  	
+					out = 0;
+      end
+	  end
+    
+
+
+  
+endmodule
+
+
+
+
 
